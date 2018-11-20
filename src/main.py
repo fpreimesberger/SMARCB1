f22d0f153628f517c0feffb5062ba7a769befd59
fpriemesberger <fpreimesberger@gmail.com>
2018-11-19 22:15:59 -0600

    niocne

--- src/main.py
+++ src/main.py
@@ -6,19 +6,27 @@ from bs4 import BeautifulSoup
 
 from Bio import Medline
 from Bio import Entrez
+Entrez.email = "fpreimesberger@gmail.com"
 
 # Scraping PubMed DBVar for pathogenic variants of SMARCB1
-Entrez.email = "fpreimesberger@gmail.com"
-handle = Entrez.esearch(db="dbvar", term='(SMARCB1[Gene Name]) pathogenic')
-records = Entrez.read(handle)
-# uids stores the unique IDs for each of the variants
-uids = records['IdList']
-print(uids)
+def getPathogenicUids():
+    handle = Entrez.esearch(db="dbvar", term='(SMARCB1[Gene Name]) pathogenic')
+    records = Entrez.read(handle)
+    # uids stores the unique IDs for each of the variants
+    pathogenic_uids = records['IdList']
+    print(pathogenic_uids)
 
-for uid in uids:
-    search = str(uid) + '[UID]'
-    print(search)
-    handle =  Entrez.esummary(db='dbvar', id=uid)
-    print(Entrez.read(handle))
+# Scraping PubMed DBVar for normal variants of SMARCB1
+def getNormalUids():
+    handle2 = Entrez.esearch(db="dbvar", term='(SMARCB1[Gene Name]) NOT pathogenic')
+    records2 = Entrez.read(handle2)
+    normal_uids = records['IdList']
+    print(normal_uids)
+
+# for uid in pathogenic_uids:
+#     search = str(uid) + '[UID]'
+#     print(search)
+#     handle =  Entrez.esummary(db='dbvar', id=uid)
+#     print(Entrez.read(handle))
 
 # Save data sets into .txt files
